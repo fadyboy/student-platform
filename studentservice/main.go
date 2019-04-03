f2a56d00e7a43d490e91947f4a210db801bea8d1
Anthony Fadero <anthonyf@ecobee.com>
2019-04-02 22:25:53 -0400

    Accept changes properly

--- studentservice/main.go
+++ studentservice/main.go
@@ -3,13 +3,8 @@ package main
 import (
 	"fmt"
 
-<<<<<<< HEAD
 	"github.com/student-platform/studentservice/dbclient"
 	"github.com/student-platform/studentservice/services"
-=======
-	"github.com/student-plaform/studentservice/dbclient"
-	"github.com/student-plaform/studentservice/services"
->>>>>>> f62371150825e90e133ee51bd9d30eec152e53e6
 )
 
 func main() {
